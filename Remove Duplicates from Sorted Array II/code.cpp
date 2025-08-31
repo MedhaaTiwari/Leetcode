75b0057e0a7591409ef10a6c59a06b2a1389cc8d
Medha Tiwari <medha.h.tiwari@gmail.com>
2025-09-01 05:02:44 +0530

    Create code.cpp

---
+++ Remove Duplicates from Sorted Array II/code.cpp
@@ -0,0 +1,13 @@
+ class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+    for(int i=nums.size()-3; i>=0; i--){
+            if (nums[i]==nums[i+2])
+            {
+                 nums.erase(nums.begin() + (i+2));
+            }
+    }  
+    return nums.size();
+    }
+    
+};
